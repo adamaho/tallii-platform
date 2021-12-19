12a3c432a7751b78271617a6c9f202a50627dfce
Adam Aho <adam.aho@hey.com>
2021-12-19 18:42:07 -0500

    adding some more stuff to the migrations

--- migrations/20210220013352_initial.sql
+++ migrations/20210220013352_initial.sql
@@ -6,4 +6,26 @@ create table users (
     created_at timestamp not null default current_timestamp
 );
 
+create table games (
+    game_id serial primary key,
+    name text not null,
+    created_at timestamp not null default current_timestamp
+);
+
+create table scoreboards (
+    scoreboard_id serial primary key,
+    name text not null,
+    game integer not null references games(game_id),
+    created_at timestamp not null default current_timestamp
+);
+
+create table teams (
+    team_id serial primary key,
+    scoreboard_id not null references scoreboards(scoreboard_id) on delete cascade
+    name text not null,
+    created_at timestamp not null default current_timestamp,
+    unique(team_id, scoreboard_id)
+);
+
+
 
